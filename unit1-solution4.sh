710700dd028491d872aa49c523bd118446e1b92b
hugh0269 <hugh0269@flinders.edu.au>
2020-06-22 00:13:14 +0930

    'sunday night' all file push

---
+++ unit1-solution4.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+sudo mkdir unit1exercise2
+cd unit1exercise2
+mkdir -m 507 angetritter
+mkdir -m 610 aushundheit
+mkdir -m 653 aufgewarfs
+mkdir -m 353 besprachtest
+mkdir -m 073 enpflums
+mkdir -m 012 enpflumen
+mkdir -m 711 verklettt
+mkdir -m 603 verrennkeit
+
+sudo mkdir -m 173 "enpflumen/aufgesitzse"
+sudo mkdir -m 227 "enpflumen/aufgesitzse/zerraucher"
+sudo mkdir -m 243 "enpflumen/beklettte"
+
+sudo mkdir -m 725 "verklettt/aufwarfheit"
+sudo mkdir -m 337 "verklettt/aufwarfheit/enwarfst"
+sudo mkdir -m 341 "verklettt/aufwarfheit/bestehs"
+
+chown news:tape angetritter
+chown news:student aushundheit
+chown mail:floppy aufgewarfs
+chown proxy:fax besprachtest
+chown news:fax enpflums
+chown news:news enpflumen
+chown news:cdrom "enpflumen/aufgesitzse"
+chown nobody:tape "enpflumen/aufgesitzse"
+chown mail:student "enpflumen/beklettte"
+chown proxy:tape verklettt
+chown proxy:voice "verklettt/aufwarfheit"
+chown games:tape "verklettt/aufwarfheit/enwarfst"
+chown news:fax "verklettt/aufwarfheit/bestehs"
+chown news:floppy verrennkeit
+
+cd angetritter
+sudo mkdir -m 507 "angetritter/antrittung"
+sudo mkdir -m 116 "angetritter/antrittung/ankaeser"
+sudo mkdir -m 034 "angetritter/beschmeckung"
+sudo mkdir -m 323 "angetritter/beschmeckung/ausgesprachs"
+sudo mkdir -m 721 "angetritter/beschmeckung/ausrauchheit"
+sudo mkdir -m 420 "angetritter/beschmeckung/aufkletts"
+
+chown mail:student "angetritter/antrittung"
+chown games:dip "angetritter/antrittung/ankaeser"
+chown lp:audio "angetritter/beschmeckung"
+chown news:student "angetritter/beschmeckung/ausgesprachs"
+chown nobody:cdrom "angetritter/beschmeckung/ausrauchheit"
+chown proxy:floppy "angetritter/beschmeckung/aufkletts"
+
+
+tar -cvzf name.tgz /home/josh/.ssh/ENGR3891-hugh0269/unit1exercise2

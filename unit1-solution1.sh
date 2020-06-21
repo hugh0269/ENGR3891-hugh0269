389175ca024a6bc03e7adc70e156d1ea1f8160aa
hugh0269 <hugh0269@flinders.edu.au>
2020-06-21 21:08:04 +0930

    Unit1 - 1 submission

---
+++ unit1-solution1.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+sudo mkdir -v unit1exercise1
+cd unit1exercise1
+
+mkdir -m 774 -v verkaesen
+mkdir -m 660 -v enhundst
+mkdir -m 003 -v bepflumheit
+mkdir -m 072 -v zersinnse
+mkdir -m 316 -v besprachst
+mkdir -m 500 -v auswitzer
+mkdir -m 400 -v verrabarbst
+mkdir -m 626 -v zertritter
+
+sudo mkdir -m 002 -v "auswitzer/angewarfen"
+sudo mkdir -m 002 -v "auswitzer/angewarfen/aufgesetzheit"
+sudo mkdir -m 446 -v "auswitzer/angetrast"
+sudo mkdir -m 231 -v "auswitzer/angetrast/auspflumse"
+sudo mkdir -m 762 -v "auswitzer/aufgerabarbt"
+sudo mkdir -m 162 -v "auswitzer/aufgerabarbt/einkrauen"
+sudo mkdir -m 336 -v "auswitzer/aufgerabarbt/befahrt"
+
+sudo mkdir -m 507 -v "verkaesen/aufwarftest"
+sudo mkdir -m 205 -v "verkaesen/aufwarftest/aufgerennug"
+sudo mkdir -m 767 -v "zertritter/aufklettheit"
+sudo mkdir -m 212 -v "zertritter/aufklettheit/angerabarbheit"
+sudo mkdir -m 562 -v "zertritter/aufklettheit/angekrauung"
+
+tar -cvzf name.tgz /home/josh/.ssh/ENGR3891-hugh0269/unit1exercise1

999f69c67df7877129c04daf1bf20903d9d4ceae
hugh0269 <hugh0269@flinders.edu.au>
2020-06-21 21:12:20 +0930

    Unit1 - 1 submission -rev1

--- unit1-solution1.sh
+++ unit1-solution1.sh
@@ -1,28 +1,24 @@
 #!/bin/sh
-sudo mkdir -v unit1exercise1
+sudo mkdir unit1exercise1
 cd unit1exercise1
-
-mkdir -m 774 -v verkaesen
-mkdir -m 660 -v enhundst
-mkdir -m 003 -v bepflumheit
-mkdir -m 072 -v zersinnse
-mkdir -m 316 -v besprachst
-mkdir -m 500 -v auswitzer
-mkdir -m 400 -v verrabarbst
-mkdir -m 626 -v zertritter
-
-sudo mkdir -m 002 -v "auswitzer/angewarfen"
-sudo mkdir -m 002 -v "auswitzer/angewarfen/aufgesetzheit"
-sudo mkdir -m 446 -v "auswitzer/angetrast"
-sudo mkdir -m 231 -v "auswitzer/angetrast/auspflumse"
-sudo mkdir -m 762 -v "auswitzer/aufgerabarbt"
-sudo mkdir -m 162 -v "auswitzer/aufgerabarbt/einkrauen"
-sudo mkdir -m 336 -v "auswitzer/aufgerabarbt/befahrt"
-
-sudo mkdir -m 507 -v "verkaesen/aufwarftest"
-sudo mkdir -m 205 -v "verkaesen/aufwarftest/aufgerennug"
-sudo mkdir -m 767 -v "zertritter/aufklettheit"
-sudo mkdir -m 212 -v "zertritter/aufklettheit/angerabarbheit"
-sudo mkdir -m 562 -v "zertritter/aufklettheit/angekrauung"
-
+mkdir -m 774 verkaesen
+mkdir -m 660 enhundst
+mkdir -m 003 bepflumheit
+mkdir -m 072 zersinnse
+mkdir -m 316 besprachst
+mkdir -m 500 auswitzer
+mkdir -m 400 verrabarbst
+mkdir -m 626 zertritter
+sudo mkdir -m 002 "auswitzer/angewarfen"
+sudo mkdir -m 002 "auswitzer/angewarfen/aufgesetzheit"
+sudo mkdir -m 446 "auswitzer/angetrast"
+sudo mkdir -m 231 "auswitzer/angetrast/auspflumse"
+sudo mkdir -m 762 "auswitzer/aufgerabarbt"
+sudo mkdir -m 162 "auswitzer/aufgerabarbt/einkrauen"
+sudo mkdir -m 336 "auswitzer/aufgerabarbt/befahrt"
+sudo mkdir -m 507 "verkaesen/aufwarftest"
+sudo mkdir -m 205 "verkaesen/aufwarftest/aufgerennug"
+sudo mkdir -m 767 "zertritter/aufklettheit"
+sudo mkdir -m 212 "zertritter/aufklettheit/angerabarbheit"
+sudo mkdir -m 562 "zertritter/aufklettheit/angekrauung"
 tar -cvzf name.tgz /home/josh/.ssh/ENGR3891-hugh0269/unit1exercise1
